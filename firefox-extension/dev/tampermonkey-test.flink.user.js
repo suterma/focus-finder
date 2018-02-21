450bc4ada12863757ed14989aa18ab93b02d0a4e
Marcel <github.j6GgecBwKkxaNg55CDPr.marcel@codeministry.ch>
2018-02-21 23:15:21 +0100

    Release 1.3
    Finalized, Packaged and Released Version 1.3

--- firefox-extension/dev/tampermonkey-test.flink.user.js
+++ firefox-extension/dev/tampermonkey-test.flink.user.js
@@ -17,7 +17,7 @@
 // ==UserScript==
 // @name         Flink-DEV
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.3
 // @description  Adds unobtrusive fragment links to and for every header and span element (with an id attribute) on a web page.
 // @author       marcel@codeministry.ch
 // @match        *://*/*
@@ -39,6 +39,7 @@
     }
 
     //Add flink css (this is a minified version of the stylish-test.flink.css file)
+    //Credit: Minified with https://cssminifier.com/
     //addGlobalStyle(".flink-target{position:relative;overflow:visible}:link:hover.flink-anchor{text-decoration:none}.flink-anchor-icon{position:absolute;bottom:.2em;width:2em;background-image:url(\"data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8' viewBox='0 0 8 8'%3E%3Cpath d='M5.88.03c-.18.01-.36.03-.53.09-.27.1-.53.25-.75.47a.5.5 0 1 0 .69.69c.11-.11.24-.17.38-.22.35-.12.78-.07 1.06.22.39.39.39 1.04 0 1.44l-1.5 1.5c-.44.44-.8.48-1.06.47-.26-.01-.41-.13-.41-.13a.5.5 0 1 0-.5.88s.34.22.84.25c.5.03 1.2-.16 1.81-.78l1.5-1.5c.78-.78.78-2.04 0-2.81-.28-.28-.61-.45-.97-.53-.18-.04-.38-.04-.56-.03zm-2 2.31c-.5-.02-1.19.15-1.78.75l-1.5 1.5c-.78.78-.78 2.04 0 2.81.56.56 1.36.72 2.06.47.27-.1.53-.25.75-.47a.5.5 0 1 0-.69-.69c-.11.11-.24.17-.38.22-.35.12-.78.07-1.06-.22-.39-.39-.39-1.04 0-1.44l1.5-1.5c.4-.4.75-.45 1.03-.44.28.01.47.09.47.09a.5.5 0 1 0 .44-.88s-.34-.2-.84-.22z' /%3E%3C/svg%3E\");background-size:.7em;background-repeat:no-repeat;background-position:center;transform:rotate(0)}.flink-target>.flink-anchor{display:none}.flink-target:hover>.flink-anchor{display:inline-flex}");
 
     //Select the elements to flinkify
